8494cd14e3327b244b3d2c8514eedb1685d4437f
LemonNeko <chheese048@gmail.com>
2020-06-30 17:12:18 +0800

    更改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,17 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ]
+if (process.env.NODE_ENV === "production") {
+    const path = require("path")
+    module.exports = {
+        "transpileDependencies": [
+            "vuetify"
+        ],
+        outputDir: "doc",
+        assetsDir: "static",
+        productionSourceMap: true
+    }
+} else if (process.env.NODE_ENV === "development") {
+    module.exports = {
+        "transpileDependencies": [
+            "vuetify"
+        ]
+    }
 }
