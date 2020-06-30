02dcf924752da307bce0570e9fc54f0136d9d2d0
LemonNeko <chheese048@gmail.com>
2020-06-30 17:32:55 +0800

    把静态资源文件移出各自的文件夹

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
 if (process.env.NODE_ENV === "production") {
-    const path = require("path")
     module.exports = {
         "transpileDependencies": [
             "vuetify"
         ],
         outputDir: "docs",
         assetsDir: "./",
+        runtimeCompiler: false,
         productionSourceMap: true
     }
 } else if (process.env.NODE_ENV === "development") {
